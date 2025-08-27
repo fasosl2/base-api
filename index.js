b3239f16e768fe423c67f40c9086d79d6304d476
fasosl2 <faso.sl2@gmail.com>
2025-08-27 12:41:18 -0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const fs = require('fs');
-const https = require('https');
+/**
+ * Arquivo: index.js
+ * Descrição: Responsável pelo Back-End da Aplicação
+ * Data: 03/02/2020
+ * Autor: Flávio Oliveira
+ */
+
 const app = require('./src/app');
 
 const port = process.env.PORT || 8000;
 
-// Carregar certificado autoassinado
-const options = {
-  key: fs.readFileSync('./src/certs/key.pem'),
-  cert: fs.readFileSync('./src/certs/cert.pem')
-};
-
-// Rodar servidor HTTPS
-https.createServer(options, app).listen(port, '0.0.0.0', () => {
-  console.log(`Executando em https://0.0.0.0:${port}`);
-});
+app.listen(port, '0.0.0.0', () => console.log(`Executando em http://0.0.0.0:${port}`));
